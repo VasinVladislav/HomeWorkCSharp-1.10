c8723903c8159730c2ca0e513860a8cdffa05582
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 13:06:53 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.10. Вывести на экран числа 5, 10 и 21 одно под другим.
+            int five = 5;
+            int ten = 10;
+            int twentyOne = 21;
+            Console.WriteLine($"{five}\n{ten}\n{twentyOne}");
+        }
+    }
+}
